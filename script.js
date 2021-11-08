b2832c2a625b556244315fc4374c1f83edc014d2
huutuan1997 <88965640+huutuan1997@users.noreply.github.com>
2021-11-08 10:29:09 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Basic initialization is like this:
+// $('.your-class').slick();
+
+// I added some other properties to customize my slider
+// Play around with the numbers and stuff to see
+// how it works.
+$('.slick-carousel').slick({
+  infinite: true,
+  slidesToShow: 5, // Shows a three slides at a time
+  slidesToScroll: 1, // When you click an arrow, it scrolls 1 slide at a time
+ 
+	
+});
+
